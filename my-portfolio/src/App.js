671fac9c0ff5760140c4f726c3658f8b7c7e3d31
Sathya <7priyavaidhy@gmail.com>
2025-02-23 10:01:15 -0500

    First Commit

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My Portfolio</h1>
+        <p>
+          I'm Sathya Priyadharshni, a passionate developer with experience in building web applications.
+        </p>
+        <h2>Skills</h2>
+        <ul>
+          <li>React.js</li>
+          <li>Node.js</li>
+          <li>JavaScript</li>
+          <li>HTML & CSS</li>
+        </ul>
+        <h2>Projects</h2>
+        <ul>
+          <li>Project 1: My first React app</li>
+          <li>Project 2: E-commerce site</li>
+        </ul>
+        <h2>Contact</h2>
+        <p>Email: 7priyavaidhy@gmail.com</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
